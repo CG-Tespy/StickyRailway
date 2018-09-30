3e079649a48209770429bff79a500ef21846dc69
CG-Tespy <gabriel.cabrera257@hotmail.com>
2018-09-30 03:44:57 -0400

    Title Screen Stuff

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour 
+{
+	public static GameController S 				{ get; protected set; }
+	public UnityEvent GameStart 				{ get; protected set; }
+	public UnityEvent GameOver 					{ get; protected set; }
+
+	[SerializeField] float fadeOutDuration =  	3f;
+	[SerializeField] float fadeInDuration = 	3f;
+	[SerializeField] AudioClip titleMusic;
+	[SerializeField] AudioClip victoryMusic;
+	[SerializeField] AudioClip gameOverMusic;
+	[SerializeField] AudioClip stageMusic;
+	[SerializeField] string gameSceneName;
+
+	ScreenFader screenFader;
+
+	UnityAction victorySequence, deathSequence, resetGameState, fadeIntoGameScene, fadeIntoTitleScreen;
+	AudioSource musicPlayer, sfxPlayer;
+	AudioFader musicFader;
+
+
+	// Use this for initialization
+
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+	{
+		bool titleScreen = 			scene.name == "TitleScreen";
+
+		if (titleScreen)
+		{
+			// For fading into the title screen
+			if (screenFader.opacity > 0)
+				screenFader.Fade(0, 1);
+
+			// Make the start button fade into the game scene
+			Button startButton = 	GameObject.Find("StartButton").GetComponent<Button>();
+			startButton.onClick.AddListener(fadeIntoGameScene);
+		}
+
+		// Basically reset the game when loading into the game scene
+		bool gameScene = 			scene.name == "game";
+
+		if (gameScene)
+		{
+			screenFader.FadeBegin.RemoveAllListeners();
+			screenFader.FadeEnd.RemoveAllListeners();
+
+			if (screenFader.opacity > 0)
+				screenFader.Fade(0, 1);
+
+			musicPlayer.Stop();
+			musicPlayer.clip = 			stageMusic;
+			musicPlayer.PlayDelayed(1f);
+		}
+		
+		Debug.Log("On Scene Loaded!");
+
+	}
+
+	void Awake () 
+	{
+		if (S == null)
+			S = 								this;
+		else 
+			Destroy(this.gameObject);
+
+		GameStart = 							new UnityEvent();
+		GameOver = 								new UnityEvent();
+
+		screenFader = 							GameObject.FindObjectOfType<ScreenFader>();
+		musicPlayer = 							GameObject.Find("MusicPlayer").GetComponent<AudioSource>();
+		musicFader = 							musicPlayer.GetComponent<AudioFader>();
+
+		fadeIntoGameScene = 					new UnityAction(FadeIntoGameScene);
+		fadeIntoTitleScreen = 					new UnityAction(FadeIntoTitleScreen);
+
+		UnityAction<Scene, LoadSceneMode> onSceneLoaded = 	OnSceneLoaded;
+		SceneManager.sceneLoaded += 			onSceneLoaded;
+	}
+
+	void Start()
+	{
+		screenFader.Fade(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void FadeIntoTitleScreen()
+	{
+		if (!screenFader.isFading)
+		{
+			musicFader.FadeIntoClip(titleMusic, 0.8f, 1f, 1f);
+			screenFader.Fade(1, 1);
+			screenFader.FadeEnd.AddListener(() => SceneManager.LoadScene("TitleScreen"));
+		}
+	}
+
+	void FadeIntoGameScene()
+	{
+		if (!screenFader.isFading)
+		{
+			musicFader.FadeIntoClip(stageMusic, 1f, 1f, 1f);
+			screenFader.Fade(1, 1);
+			screenFader.FadeEnd.AddListener(() => SceneManager.LoadScene(gameSceneName));
+		}
+	}
+}
---
+++ Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class ScreenFader : MonoBehaviour 
+{
+	public static int count 			{ get; protected set; }
+	public UnityEvent FadeBegin 		{ get; protected set; }
+	public UnityEvent FadeEnd 			{ get; protected set; }
+	public CanvasGroup canvasGroup 		{ get; protected set; }
+	IEnumerator fadeCoroutine = 		null;
+	public bool isFading 				{ get; protected set; }
+	Image image;						
+
+	public bool blocksRaycasts
+	{
+		get { return canvasGroup.blocksRaycasts; }
+		set { canvasGroup.blocksRaycasts = value; }
+	}	
+	public bool interactable 
+	{
+		get { return canvasGroup.interactable; }
+		set { canvasGroup.interactable = value; }
+	}
+
+	public float opacity 
+	{
+		get { return canvasGroup.alpha; }
+		set { canvasGroup.alpha = value; }
+	}
+
+	// Use this for initialization
+	void Awake () 
+	{
+		// Make sure there is only one ScreenFader in a scene at a time
+		if (count > 0)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+
+		count++;
+
+		image = 						GetComponent<Image>();
+		canvasGroup = 					GetComponent<CanvasGroup>();
+		interactable = 					false;
+		blocksRaycasts = 				false;
+
+		// Make this object persist between scenes
+		Transform rootObj = 			transform;
+
+		while (rootObj.parent != null)
+			rootObj = 					rootObj.parent;
+
+		DontDestroyOnLoad(rootObj.gameObject);
+
+		FadeBegin = 					new UnityEvent();
+		FadeEnd = 						new UnityEvent();
+		isFading = 						false;
+		
+	}
+
+	void Start()
+	{
+		GameController.S.GameOver.AddListener(ResetState);
+	}
+	
+	public void Fade(float targetOpacity, float duration = 1f)
+	{
+		if (fadeCoroutine != null)
+			StopCoroutine(fadeCoroutine);
+
+		fadeCoroutine = 	FadeCoroutine(targetOpacity, duration);
+		StartCoroutine(fadeCoroutine);
+	}
+	
+	IEnumerator FadeCoroutine(float targetOpacity, float duration = 1f)
+	{
+		isFading = 			true;
+		blocksRaycasts = 	true;
+		Debug.Log("Fading!");
+		FadeBegin.Invoke();
+		float baseOpacity = opacity;
+		float timer = 		0f;
+
+		while (timer < duration)
+		{
+			timer += 		Time.deltaTime;
+			opacity = 		Mathf.Lerp(baseOpacity, targetOpacity, timer / duration);
+			Debug.Log("Current opacity: " + opacity);
+			yield return null;
+		}
+
+		// Make sure to be precise
+		opacity = 			targetOpacity;
+
+		fadeCoroutine = 	null;
+
+		// Makes it so the screen doesn't keep you from interacting with UI elements 
+		// when it's done.
+		blocksRaycasts = 	false;
+		isFading = 			false;
+		FadeEnd.Invoke();
+	}
+
+	void ResetState()
+	{
+		Debug.Log("ScreenFader state reset!");
+		if (fadeCoroutine != null)
+		{
+			Debug.Log("Stopping fade cooroutine!");
+			StopCoroutine(fadeCoroutine);
+		}
+
+		opacity = 		0;
+		FadeBegin.RemoveAllListeners();
+		FadeEnd.RemoveAllListeners();
+
+	}
+
+	
+}
